9d00e77f0b654bae69a0892aa8f197a015e84024
ssidrabilal1 <sidrabilal1105@hotmail.com>
2024-08-06 14:12:59 -0400

    all HW is done m

--- src/main/java/org/example/e86/E86ClassAndObject.java
+++ src/main/java/org/example/e86/E86ClassAndObject.java
@@ -1,30 +1,7 @@
-import org.example.e86.Student;
 
-public class E86ClassAndObject {
-    public static void main(String[] args) {
-        // Create a class named 'Student'
 
-        // Declare a String variable named 'name' and an integer variable named 'roll_no'
 
-        // Create an object of the class 'Student'
 
-        // Assign the value "John" to the 'name' variable
-
-        // Assign the value 2 to the 'roll_no' variable
-
-        // Print the values of 'name' and 'roll_no' in the specified format
-
-        // Create an object of the Student class
-        Student student = new Student();
-
-        // Assign values to the object's properties
-        student.name = "John";
-        student.roll_no = 2;
-
-        // Print the properties of the object
-        System.out.println("Name is " + student.name + " and roll number is " + student.roll_no);
-    }
-}
 
 
 

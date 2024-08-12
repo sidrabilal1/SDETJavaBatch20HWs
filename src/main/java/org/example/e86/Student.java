427fd4e600f7e878afa96d58335c83aaa8bb0d16
sidrabilal1 <sidrabilal1105@gmail.com>
2024-08-12 19:19:57 -0400

    message

--- src/main/java/org/example/e86/Student.java
+++ src/main/java/org/example/e86/Student.java
@@ -5,7 +5,7 @@ public class Student {
     // Declare a String variable named 'name'
     // Declare an integer variable named 'roll_no'
 
-    Student student = new Student();
+
     String name;
     int  roll_no;
 
